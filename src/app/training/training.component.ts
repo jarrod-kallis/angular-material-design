be3362b9093eeb496b22d18da8ed7b4a0085f48c
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-01-08 09:46:37 +0200

    Change training tab caption

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TrainingService } from './training.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.css']
 })
-export class TrainingComponent implements OnInit {
-  constructor() { }
+export class TrainingComponent implements OnInit, OnDestroy {
+  private exerciseStatusChangedSubscription: Subscription;
+  private _trainingTabCaption: string;
 
-  ngOnInit() { }
+  constructor(private trainingService: TrainingService) { }
+
+  ngOnInit() {
+    this.exerciseStatusChangedSubscription = this.trainingService.onExerciseStatusChanged
+      .subscribe(() => {
+        this._trainingTabCaption = this.trainingService.exerciseStarted ? 'Training In Progress' : 'New Training';
+      });
+  }
+
+  ngOnDestroy() {
+    this.exerciseStatusChangedSubscription.unsubscribe();
+  }
+
+  get trainingTabCaption() {
+    return this._trainingTabCaption;
+  }
 }
